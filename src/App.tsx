7fc50b00aba1d1b2576d34a6b8a913d72cbf0926
jpcchaves <joaopaulodecastrochaves@hotmail.com>
2023-02-02 13:54:14 -0300

    feat: add chakra ui dark mode toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+import { Button, Container, useColorMode } from '@chakra-ui/react';
+
 const App = () => {
-	return <div className='App'></div>;
+	const { colorMode, toggleColorMode } = useColorMode();
+
+	return (
+		<Container>
+			<Button onClick={() => toggleColorMode()}>
+				{colorMode === 'dark' ? (
+					<SunIcon color={'orange.300'} />
+				) : (
+					<MoonIcon color={'blue.300'} />
+				)}
+			</Button>
+		</Container>
+	);
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ChakraBaseProvider } from '@chakra-ui/react';
+import theme from './theme';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<ChakraBaseProvider resetCSS={true}>
+		<ChakraBaseProvider resetCSS={true} theme={theme}>
 			<App />
 		</ChakraBaseProvider>
 	</React.StrictMode>
