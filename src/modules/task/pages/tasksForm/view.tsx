f8802f2cfcba705c5464ff4e173674f9cad7baf5
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2023-02-05 12:10:39 -0300

    feat: add create task form

--- src/modules/task/pages/tasksForm/view.tsx
+++ src/modules/task/pages/tasksForm/view.tsx
@@ -1,7 +1,59 @@
-import { Container } from '@chakra-ui/react';
+import { AddIcon, SmallAddIcon } from '@chakra-ui/icons';
+import {
+	Button,
+	Container,
+	Flex,
+	FormControl,
+	FormErrorMessage,
+	FormHelperText,
+	FormLabel,
+	Heading,
+	Input,
+	InputGroup,
+	InputLeftElement,
+} from '@chakra-ui/react';
+import { Formik, Form } from 'formik';
 
 const TasksFormView = () => {
-	return <Container></Container>;
+	const isError = true;
+
+	return (
+		<Container maxW={{ lg: '3xl', md: '5xl', sm: 'container.xl' }} pt={'6'}>
+			<FormControl
+				isInvalid={isError}
+				border={'1px solid #ccc'}
+				p={'8'}
+				rounded={'md'}
+				boxShadow={'lg'}
+				isRequired
+			>
+				<Heading size='md' textAlign={'center'}>
+					Criar nova task
+				</Heading>
+				<Formik>
+					<Form>
+						<FormLabel>Task</FormLabel>
+						<Input type='text' name='task' placeholder='Digite a task...' />
+						<FormErrorMessage>A task é obrigatória</FormErrorMessage>
+
+						<FormLabel pt={'4'}>Prazo</FormLabel>
+						<Input
+							type='datetime-local'
+							name='deadline'
+							placeholder='Digite a task...'
+						/>
+						<FormErrorMessage>O prazo é obrigatório</FormErrorMessage>
+
+						<Flex mt='8' justifyContent='flex-end'>
+							<Button colorScheme='blue' type='submit'>
+								Criar
+							</Button>
+						</Flex>
+					</Form>
+				</Formik>
+			</FormControl>
+		</Container>
+	);
 };
 
 export default TasksFormView;
