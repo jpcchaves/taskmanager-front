46238d4fb6478f4b6401d891befa4910e8770e63
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2023-02-07 23:10:25 -0300

    refactor: refactoring useUpdate task mutation

--- src/hooks/useUpdateTaskMutation.tsx
+++ src/hooks/useUpdateTaskMutation.tsx
@@ -1,11 +1,21 @@
 import { useMutation } from 'react-query';
+import { TaskI } from '../modules/task/types/taskI';
 import { api } from './useApi';
 
 const useUpdateTaskMutation = () => {
+	const updateTask = async ({
+		id,
+		...formPayload
+	}: {
+		id: string | undefined;
+		formPayload: TaskI;
+	}) => {
+		return await api.put(`/v1/task/${id}`, formPayload);
+	};
+
 	const updateTaskMutation = () => {
 		return useMutation(async (formPayload) => {
-			const res = await api.put(`/v1/task/${formPayload?.id}`, formPayload);
-			return res.data;
+			return updateTask(formPayload!);
 		});
 	};
 	const { mutate, isLoading } = updateTaskMutation();
